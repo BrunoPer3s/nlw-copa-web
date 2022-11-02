0bbda25ce0ad8d09ac2b2010149cbfc38de7a5d9
BrunoPer3s <brunoperes7@hotmail.com>
2022-11-02 14:33:45 -0300

    Layout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,66 @@
-interface HomeProps {
-  count: number;
-}
+import Image from 'next/image'
+import logoImg from '../assets/logo-img.svg'
+import avatarImg from '../assets/avatar-img.png'
+import phoneImg from '../assets/phone-img.png'
+import { CheckCircle } from 'phosphor-react'
 
-export default function Home({ count }: HomeProps) {
+export default function Home() {
   return (
-    <h1 className="text-2xl text-white">{count}</h1>
+    <div className='max-w-[375px] flex items-center w-full h-screen mx-auto text-center px-4 tablet:max-w-[490px] tablet:text-left tablet:flex tablet:items-center tablet:px-0 desktop:max-w-[1124px] desktop:justify-between desktop:px-4'>
+      <main className='flex flex-col mt-4 tablet:mt-0 gap-8 tablet:gap-0 tablet:py-0  desktop:max-w-[490px]'>
+        <header className='flex justify-center tablet:justify-start'>
+          <Image src={logoImg} alt="nlw Copa" />
+        </header>
+        <h1 className='text-white text-2xl font-bold tablet:text-5xl tablet:text-start tablet:mt-[60px] tablet:mb-[40px] tablet:leading-tight'>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <div className='flex items-center justify-around tablet:mb-[40px]'>
+          <div className='flex'>
+            <Image src={avatarImg} alt="avatar" />
+            <Image className='ml-[-25px]' src={avatarImg} alt="avatar" />
+            <Image className='ml-[-25px] hidden tablet:block' src={avatarImg} alt="avatar" />
+            <Image className='ml-[-25px] hidden tablet:block' src={avatarImg} alt="avatar" />
+          </div>
+          <div>
+            <strong className='text-gray-100 tablet:text-xl'>
+              <span className='text-green-500'>+12.592 </span>
+              pessoas já estão usando
+            </strong>
+          </div>
+        </div>
+        <form className='flex flex-col gap-4 tablet:flex-row'>
+          <input
+            className='px-6 py-4 bg-gray-800 border border-gray-700 text-sm text-gray-200 rounded tablet:flex-1'
+            type="text"
+            placeholder='Qual o nome do bolão ?'
+            required
+          />
+          <button
+            className='bg-yellow-500 px-6 h-[55px] flex items-center justify-center font-bold text-black text-sm uppercase rounded'
+            type='submit'>
+            criar meu bolão
+          </button>
+        </form>
+        <p className='text-sm text-gray-600 leading-[160%] tablet:mt-[16px] tablet:mb-[40px]'>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <footer>
+          <ul className='flex justify-around items-center pt-8 border-t border-t-gray-600'>
+            <li className='flex items-center  gap-[10px] tablet:gap-6'>
+              <CheckCircle className='text-white text-[40px] tablet:text-5xl' weight='fill' fill='#129E57' />
+              <div className='flex flex-col items-start'>
+                <span className='text-gray-100 font-bold tablet:text-2xl'>+2.304</span>
+                <span className='text-gray-100 text-xs tablet:text-base'>Bolões criados</span>
+              </div>
+            </li>
+            <div className='h-14 bg-gray-600 w-px'></div>
+            <li className='flex items-center  gap-[10px] tablet:gap-6'>
+              <CheckCircle className='text-whit text-[40px] tablet:text-5xl' weight='fill' color='#129E57' />
+              <div className='flex flex-col items-start'>
+                <span className='text-gray-100 font-bold tablet:text-2xl'>+2.304</span>
+                <span className='text-gray-100 text-xs tablet:text-base'>Bolões criados</span>
+              </div>
+            </li>
+          </ul>
+        </footer>
+      </main>
+      <Image className='hidden desktop:block' src={phoneImg} alt="Imagem de previsão" />
+    </div>
   )
-}
-
-export const getServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/pools/count')
-  const data = await response.json()
-
-  return {
-    props: {
-      count: data.poolsCount
-    }
-  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,34 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    screens: {
+      'tablet': '650px',
+      'desktop': '1050px'
+    },
     extend: {
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
       colors: {
         gray: {
-          900: '#121214'
+          900: '#121214',
+          800: '#202024',
+          700: '#323238',
+          600: '#8d8d99',
+          200: '#c4c4cc',
+          100: '#e1e1e6'
+        },
+        green: {
+          500: '#129e57'
+        },
+        yellow: {
+          500: '#f7dd43'
+        },
+        blue: {
+          500: '#20234F'
         }
       }
     },
